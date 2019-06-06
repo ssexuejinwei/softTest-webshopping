2e9760ee7932af4b1b98982650b0d3b4b1bc48b6
xuejinwei <xue@example.com>
2019-06-06 23:48:09 +0800

    after checkstyle

--- src/main/java/com/shopping/service/UserDetailServiceImplement.java
+++ src/main/java/com/shopping/service/UserDetailServiceImplement.java
@@ -2,39 +2,39 @@ package com.shopping.service;
 
 import com.shopping.dao.UserDetailDao;
 import com.shopping.entity.UserDetail;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 public class UserDetailServiceImplement implements UserDetailService {
+  @Autowired
+  private UserDetailDao userDetailDao;
 
-    @Autowired
-    private UserDetailDao userDetailDao;
+  @Override
+  public UserDetail getUserDetail(int id) {
+    return userDetailDao.getUserDetail(id);
+  }
 
-    @Override
-    public UserDetail getUserDetail(int id) {
-        return userDetailDao.getUserDetail(id);
-    }
+  @Override
+  public void addUserDetail(UserDetail userDetail) {
+    userDetailDao.addUserDetail(userDetail);
+  }
 
-    @Override
-    public void addUserDetail(UserDetail userDetail) {
-        userDetailDao.addUserDetail(userDetail);
-    }
+  @Override
+  public boolean deleteUserDetail(int id) {
+    return userDetailDao.deleteUserDetail(id);
+  }
 
-    @Override
-    public boolean deleteUserDetail(int id) {
-        return userDetailDao.deleteUserDetail(id);
-    }
+  @Override
+  public boolean updateUserDetail(UserDetail userDetail) {
+    return userDetailDao.updateUserDetail(userDetail);
+  }
 
-    @Override
-    public boolean updateUserDetail(UserDetail userDetail) {
-        return userDetailDao.updateUserDetail(userDetail);
-    }
-
-    @Override
-    public List<UserDetail> getAllUserDetail() {
-        return userDetailDao.getAllUserDetail();
-    }
+  @Override
+  public List<UserDetail> getAllUserDetail() {
+    return userDetailDao.getAllUserDetail();
+  }
 }
